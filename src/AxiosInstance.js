accd8fdfbd5066ed29025a01abeba173424074b4
teojincheng <jinchengproject@gmail.com>
2020-04-23 11:23:29 +0800

    [jc] update axiosInstance to include URL to backend hosted on  heroku

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  //baseURL: "http://localhost:3000",
+  baseURL: "https://radiant-sierra-26169.herokuapp.com",
 });
 
 instance.defaults.headers.post["Content-Type"] = "application/json";
